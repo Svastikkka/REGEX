5d4d8b67ec08333d32e4f488150cd6a1b43e4ac8
Manshu Sharma <36654754+Svastikkka@users.noreply.github.com>
2020-12-20 19:12:24 +0530

    Update testScrapping.py

--- testScrapping.py
+++ testScrapping.py
@@ -1,18 +1,23 @@
 import re
 arr=[]
-with open("internship.routes.js", "r") as f1:
+with open("admin.routes.js", "r") as f1:
     for i in f1.readlines():
         if re.search("const",i):
             continue
         elif re.search("//",i):
             print(i)
-            #arr.append(i)
         else:
             x = re.search('([/])\w+', i)
-            if x:
-                arr.append(x.group())
+            y = re.search("post", i)
+            z = re.search("get", i)
+            if x and y:
+                arr.append(x.group()+'", "method": "'+y.group())
+            if x and z:
+                arr.append(x.group()+'", "method": "'+z.group())
+
+print(arr)
 size=len(arr)
-with open("result.json","a") as f2:
+with open("admin.json","a") as f2:
     f2.writelines('[')
     f2.write("\n")
     for i  in arr:
